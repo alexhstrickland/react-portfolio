008960fec83cb7e57cca926d4d71092060d14cf2
alexhstrickland <alexhstrickland@gmail.com>
2021-04-27 21:35:16 -0400

    added pages folder and deleted extra files

---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,78 @@
+
+function Portfolio() {
+    return (
+        <main className="container">
+            <h1>Portfolio</h1>
+            <section className="wrapper">
+                <section className="row">
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">Posts N' Stuff</h2>
+                            <p className="card-text"> For this project, I worked alongside three other team members to create an application where a user can register and sign in to a profile where they can create,like, and dislike posts from drinks, food, and diy projects.
+                            </p>
+                            <a href="https://github.com/JFunck1990/Posts-n-Stuff.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="https://postnstuff.herokuapp.com/" id="Refactor" className="btn btn-primary">Live URL</a>
+                        </section>
+                        </section>
+                    </section>
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">Note Taker</h2>
+                            <p className="card-text"> For this project, I created an application called Note Taker that can be used to write and save notes. The application will use an Express.js back end and will save and retrieve data from a JSON file.
+
+                            </p>
+                            <a href="https://github.com/alexhstrickland/Note_Taker.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="https://notelist-taker-app.herokuapp.com/" id="Refactor" className="btn btn-primary">Live URL</a>
+                        </section>
+                        </section>
+                    </section>
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">Team Profile Generator</h2>
+                            <p className="card-text"> For this project, I created a command-line application that takes in user input about employees on a software engineering team, then generates an HTML webpage displaying a card for each employee.</p>
+                            <a href="https://github.com/alexhstrickland/Team_Profile_Generator.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="https://drive.google.com/file/d/1RGEc_5pBQUNxRwyp3G-egPaOnpf7I_lH/view?usp=sharing" id="Refactor" className="btn btn-primary">Live DEMO</a>
+                        </section>
+                        </section>
+                    </section>
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">README Generator</h2>
+                            <p className="card-text"> For this project, I created an application that uses a command line tool to generate a professional README file based on the users input.</p>
+                            <a href="https://github.com/alexhstrickland/README_Generator.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="https://drive.google.com/file/d/1UZql367xDXxexfsyuAd-DlOHWx4r_acU/view?usp=sharing" id="Refactor" className="btn btn-primary">Live DEMO</a>
+                        </section>
+                        </section>
+                    </section>
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">Server-Side API Project</h2>
+                            <p className="card-text"> For this project, I worked alongside 3 other team members to create an application that displays data received from multiple server-side API requests. </p>
+                            <a href="https://github.com/kmh8827/project1-team2.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="http://kmh8827.github.io/project1-team2" id="Refactor" className="btn btn-primary">Live URL</a>
+                        </section>
+                        </section>
+                    </section>
+                    <section className="col-md-4">
+                        <section className="card">
+                        <section className="card-body">
+                            <h2 className="card-title">Weather Dashboard</h2>
+                            <p className="card-text"> For this project, I created a weather dashboard that displays the current and future weather forecast for a city entered by the user.</p>
+                            <a href="https://github.com/alexhstrickland/Weather_API.git" id="Refactor" className="btn btn-primary">Github Repo</a>
+                            <a href="https://alexhstrickland.github.io/Weather_API/" id="Refactor" className="btn btn-primary">Live URL</a>
+                        </section>
+                    </section>
+                </section>
+                
+            </section>
+            </section>
+            </main>
+    );
+}
+
+export default Portfolio;
